dd48720ec600b05231a784711254a2df3e291f94
luis amorim <lamorim@scopus.com.br>
2017-10-17 09:41:07 -0200

    first commit

---
+++ src/main/java/br/com/amorim/UiAlertComponentApplication.java
@@ -0,0 +1,12 @@
+package br.com.amorim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UiAlertComponentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UiAlertComponentApplication.class, args);
+	}
+}
---
+++ src/main/resources/static/index.js
@@ -0,0 +1,4 @@
+angular.module('app',[])
+	.controller('defaultCtrl',function($scope){
+		$scope.title = "Angular & Bootstrap";
+	});
